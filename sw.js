6a759e05e3daa81d3c1d4e60d556c3adf3500d9c
Liamautomation <liam.nisbet2004@gmail.com>
2026-07-06 16:02:49 +0700

    vocab: +55 mots Leçon 9 (temps/rendez-vous) & Bài 10 (voyage/hôtel), SW v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'viet-v24';
+const CACHE = 'viet-v25';
 const FILES = [
     './index.html', './manifest.json', './icon.svg',
     'https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js'
